a1481182c60bc58c3a3b0d95f6887479f95b9084
Rafael Silverio <fael_st@hotmail.com>
2022-12-07 17:30:07 -0300

    remove dist path

--- dist/main/routes/restaurantsRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const adapters_1 = require("@/main/adapters");
-const factories_1 = require("@/main/factories");
-exports.default = (router) => {
-    router.get('/restaurant', (0, adapters_1.adaptRoute)((0, factories_1.makeGetRestaurantsAvailableController)()));
-};
-//# sourceMappingURL=restaurantsRoutes.js.map
